80c68a99cc3dc416dbbad2ea5feaa65ab3cccea1
cezary-walczak <cezary.walczak.pl@gmail.com>
2018-01-09 18:46:57 +0100

    mixins

--- src/App.vue
+++ src/App.vue
@@ -18,15 +18,30 @@ export default {
   lightGray = #eee
   red = #be2525
 
+  border-radius(radius = 10px)
+    -webkit-border-radius radius
+    border-radius radius
+
+  clear-fix()
+    zoom 1
+    &:after
+    &:before
+      content ""
+      display table
+    &:after
+      clear both
+
   section
     width 200px
     height 200px
     background lightGray
     color red
-    border solid 10px @color
+    border solid 1px @color
     div
       width 100px
       height 100px
-      border solid 10px @color
+      border solid 1px @color
+      border-radius 30px
+      clear-fix()
 
 </style>

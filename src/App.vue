19a594a1f08e111c52496fa4598a589ef4cff5b9
cezary-walczak <cezary.walczak.pl@gmail.com>
2018-01-10 02:47:02 +0100

    clearing floats

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,22 @@
 <template lang="pug">
   #main
     .wrapper
-      img(src='./assets/logo.png' )
-      p.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-        Consequatur inventore omnis soluta voluptates, enim magnam quis a ullam. 
-        Reiciendis dolor laboriosam velit aperiam deserunt distinctio impedit ad commodi sapiente aliquid!
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-        Consequatur inventore omnis soluta voluptates, enim magnam quis a ullam. 
-        Reiciendis dolor laboriosam velit aperiam deserunt distinctio impedit ad commodi sapiente aliquid!
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-        Consequatur inventore omnis soluta voluptates, enim magnam quis a ullam. 
-        Reiciendis dolor laboriosam velit aperiam deserunt distinctio impedit ad commodi sapiente aliquid!
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-        Consequatur inventore omnis soluta voluptates, enim magnam quis a ullam. 
-        Reiciendis dolor laboriosam velit aperiam deserunt distinctio impedit ad commodi sapiente aliquid!
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-        Consequatur inventore omnis soluta voluptates, enim magnam quis a ullam. 
-        Reiciendis dolor laboriosam velit aperiam deserunt distinctio impedit ad commodi sapiente aliquid!
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-        Consequatur inventore omnis soluta voluptates, enim magnam quis a ullam. 
-        Reiciendis dolor laboriosam velit aperiam deserunt distinctio impedit ad commodi sapiente aliquid!
-
-      .box.blue
-      .box.red
-      .box.green
-
-      .service
-        h2 Service 1
-        p This is service 1
-      .service
-        h2 Service 2
-        p This is service 2
-      .service
-        h2 Service 3
-        p This is service 3
-
+      .services
+        .service
+          h2 Service 1
+          p This is service 1
+        .service
+          h2 Service 2
+          p This is service 2
+        .service
+          h2 Service 3
+          p This is service 3
+      .content
+        h2 content
+        p.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+          Consequatur inventore omnis soluta voluptates, enim magnam quis a ullam. 
+          Reiciendis dolor laboriosam velit aperiam deserunt distinctio impedit ad commodi sapiente aliquid!
 </template>
 
 <script>
@@ -50,42 +31,37 @@ export default {
 </script>
 
 <style lang="stylus">
+  clear()
+    &:after
+      content ""
+      display block
+      clear both
+
   .wrapper
     background #eee
     max-width 960px
     margin 0 auto
     padding 20px
-    &:after
-      content ""
-      display block
-      clear both
-    img
-      float right
-      margin 10px
+    clear()
 
-    .box
-      width: 100px
-      height 100px
-    .blue
-      background blue
-      float right
-    .red
-      background red
-      float left
-    .green
-      background green
-      margin auto
+    .services
+      clear()
 
-    .service
-      float left
-      background #ddd
-      width 29%
-      margin 1%
-      padding 1%
-      text-align center
-      h1, p
-        color #999
-        margin 10px 0
+      .service
+        float left
+        background #ddd
+        width 29%
+        margin 1%
+        padding 1%
         text-align center
+        h1, p
+          color #999
+          margin 10px 0
+          text-align center
 
+    .content
+      padding 10px
+      background #ccc
+      clear both
+      margin-top 100px
 </style>

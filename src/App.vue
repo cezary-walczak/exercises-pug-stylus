1dcdd7997e49c8f74c1b8c43c380e93c283390fa
cezary-walczak <cezary.walczak.pl@gmail.com>
2018-01-11 20:09:21 +0100

    axis

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,6 @@
 <template lang="pug">
   #main
     .wrapper
-      nav
-        ul
-          li
-            a(href='') menu-item-1
-          li
-            a(href='') menu-item-2
-          li
-            a(href='') menu-item-3
-          li
-            a(href='') menu-item-4
-        ul
-          li
-            a(href='').icon submenu-item-1
-          li
-            a(href='').icon submenu-item-2
-
       .flex-container
         .box.one
         .box.two
@@ -36,52 +20,23 @@ export default {
 </script>
 
 <style lang="stylus">
-
-min-screen = 800px
-
   .wrapper
     width 100%
-    nav 
-      background #333
-      ul
-        list-style-type none 
-        padding 0
-        margin 0
-        li
-          a
-            text-decoration none 
-            text-align center
-            color #fff
-            display block
-            padding 10px
-            background #666
-            &:hover
-              background #999
-            &.icon
-              background url('./assets/logo.png') no-repeat center center / contain
-              color transparent
-              text-indent -10000px
-
     .flex-container
+      background #eee
       display flex
       flex-wrap wrap
+      flex-flow column-reverse
+      justify-content center
+      height 800px
       .box
         height 100px
-        flex 1 0 400px
+        flex 0 0 100px
         &.one
           background red
         &.two
           background blue
         &.three
           background green
-
-@media screen and (min-width min-screen)
-  nav
-    display flex
-    justify-content space-between
-    ul
-      display flex
-      li
-        flex 1 1 0
 
 </style>

80291b1a52a2366448aae2ed108114ff77fe5557
cezary-walczak <cezary.walczak.pl@gmail.com>
2018-01-12 18:08:13 +0100

    nested grids

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,11 @@
       .div2 2
       .div3 3
       .div4 4
-      .div5 5
+      .div5.nested
+        p 1
+        p 2
+        p 3
+        p 4
       .div6 6
 
 </template>
@@ -26,29 +30,20 @@ export default {
     max-width 960px
     margin 0 auto
     display grid
-    grid-template-columns repeat(6, 1fr)
-    grid-template-rows repeat(4, minmax(150px, auto))
+    grid-template-columns repeat(3, 1fr)
     grid-gap 10px
     div
       background #eee
       padding 30px
       &:nth-child(even)
         background #ddd
-      &.div1
-        // grid-column-start 1
-        // grid-column-end 3 
-        grid-column 1/3
-      &.div2
-        grid-column 3/7
-      &.div3
-        grid-column 1/4
-        grid-row 2/4
-      &.div4
-        grid-column 4/7
-        grid-row 2/4
-      &.div5
-        grid-column 3/7
-      &.div6
-        grid-row 4
-        grid-column 1/3
+      &.nested
+        display grid
+        grid-template-columns 1fr 1fr
+        grid-gap 10px
+        grid-column span 3
+        p
+          border 1px solid #fff
+          padding 10px
+          margin 0
 </style>

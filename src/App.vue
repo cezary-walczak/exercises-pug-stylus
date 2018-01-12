98bd55e6bc4cca4cae52397cda94629cddf792e7
cezary-walczak <cezary.walczak.pl@gmail.com>
2018-01-12 20:11:57 +0100

    12column grid

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template lang="pug">
   #main
+    input(type='checkbox' v-on:click='gridShow = !gridShow')
+
     .wrapper
-      .div1 1
-      .div2 2
-      .div3 3
-      .div4 4
-      .div5 5
-      .div6 6
+      header header
+      main main
+      section section
+      aside aside
+      nav nav
+      footer footer
+
+      .grid(v-bind:class='{ showGrid: gridShow }')
+        p
+        p
+        p
+        p
+        p
+        p
+        p
+        p
+        p
+        p
+        p
+        p
 
 </template>
 
@@ -15,7 +31,7 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      gridShow: false
     }
   }
 }
@@ -26,18 +42,45 @@ export default {
     max-width 960px
     margin 0 auto
     display grid
-    grid-template-columns repeat(3, 1fr)
-    grid-auto-rows (minmax(150px, auto))
+    grid-template-columns repeat(12, 1fr)
+    grid-auto-rows minmax(100px auto)
     grid-gap 10px
-    // align-items start
-    justify-items streth
-    align-items streth
-    div
+    position relative
+    *
       background #eee
       padding 30px
       &:nth-child(even)
         background #ddd
-      &.div5
-        justify-self center
-        align-self center
+    header
+      grid-column 1/13
+    main
+      grid-column 4/13
+      grid-row 2/4
+    section 
+      grid-column 1/13
+      grid-row 4/6
+    aside
+    nav
+      grid-column 1/4
+    footer
+      grid-column 1/13
+
+    .grid
+      display grid
+      position absolute
+      left 0
+      top 0
+      grid-template-columns repeat(12, 1fr)
+      grid-auto-rows minmax(100% auto)
+      width 100%
+      height 100%
+      padding 0
+      opacity 0.1
+      display none
+      p
+        border 1px solid #666
+
+    .showGrid
+      display grid
+
 </style>

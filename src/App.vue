b04ba56be25ae7fb2d62d3dceec03190ce83a81d
cezary-walczak <cezary.walczak.pl@gmail.com>
2018-01-12 17:28:35 +0100

    rows

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       div 2
       div 3
       div 4
-      div 5
+      div Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam ea rem consectetur saepe accusantium est expedita doloremque suscipit ut quidem, illum architecto facilis. Illum quia quidem architecto repellendus? Deleniti, incidunt. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam ea rem consectetur saepe accusantium est expedita doloremque suscipit ut quidem, illum architecto facilis. Illum quia quidem architecto repellendus? Deleniti, incidunt.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam ea rem consectetur saepe accusantium est expedita doloremque suscipit ut quidem, illum architecto facilis. Illum quia quidem architecto repellendus? Deleniti, incidunt.
       div 6
       div 7
       div 8
@@ -32,6 +32,12 @@ export default {
     // grid-template-columns 30% 20% 50%
     // grid-template-columns 1fr 2fr 1fr
     grid-template-columns repeat(3, 1fr)
+    // grid-auto-rows minmax(200px, auto)
+    // grid-template-rows 100px 200px 300px
+    grid-template-rows repeat(3, minmax(200px, auto))
+    // grid-column-gap 10px
+    // grid-row-gap 10px
+    grid-gap 10px
     div
       background #eee
       padding 30px

e1a1c6bbc56c85c6a8667bd7f62668458ea8c1a6
cezary-walczak <cezary.walczak.pl@gmail.com>
2018-01-12 02:43:02 +0100

    basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,48 @@
 <template lang="pug">
   #main
     .wrapper
-      .flex-container
-        .box.one
-        .box.two
-        .box.three
+      header
+        img(src='./assets/logo.png')
+
+      form
+        p You are a...
+        input#male(type='radio' name='gender')
+        label(for='male') male
+        br
+        input#female(type='radio' name='gender')
+        label(for='female') female
+
+        p ...who is interested in...
+        input#web(type='checkbox')
+        label(for='web') web development
+        br
+        input#photoshop(type='checkbox')
+        label(for='photoshop') photoshop
+        br
+        input#another(type='checkbox')
+        label(for='another') doing weird things
+
+        p contact details
+        fieldset 
+          legend contact-info
+          input(
+            type='email' 
+            placeholder='email' 
+            required
+          )
+          span.tick
+          input(
+            type='telephone' 
+            placeholder='phone number' 
+            required
+          )
+          span.tick
+
+        p how would you like to contact?
+        select
+          option by email
+          option by telephone
+        input(type='submit' value='become a ninja')
 
 </template>
 
@@ -20,23 +58,22 @@ export default {
 </script>
 
 <style lang="stylus">
-  .wrapper
-    width 100%
-    .flex-container
-      display flex
-      flex-wrap wrap
-      justify-content space-between
-      .box
-        height 100px
-        flex-basis 100px
-        &.one
-          background red
-          order 0
-        &.two
-          background blue
-          order 2
-        &.three
-          background green
-          order -2
+dp = 20px
+
+  #main
+    background #eee
+    margin 0
+    font-family arial
+    .wrapper
+      header
+        background #ccc
+        padding dp
+        text-align center
+
+      form
+        padding dp
+        p
+          font-size 2rem
+          letter-spacing 0.2rem
 
 </style>

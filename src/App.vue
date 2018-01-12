9d9369cf996ea675a09c73abb2e1d0f0b13cf83f
cezary-walczak <cezary.walczak.pl@gmail.com>
2018-01-12 20:33:33 +0100

    mosaic layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,11 @@
 <template lang="pug">
   #main
-    input(type='checkbox' v-on:click='gridShow = !gridShow')
-
     .wrapper
-      header header
-      main main
-      section section
-      aside aside
-      nav nav
-      footer footer
-
-      .grid(v-bind:class='{ showGrid: gridShow }')
-        p
-        p
-        p
-        p
-        p
-        p
-        p
-        p
-        p
-        p
-        p
-        p
+      .div1 1
+      .div2 2
+      .div3 3
+      .div4 4
+      .div5 5
 
 </template>
 
@@ -42,45 +25,29 @@ export default {
     max-width 960px
     margin 0 auto
     display grid
-    grid-template-columns repeat(12, 1fr)
-    grid-auto-rows minmax(100px auto)
+    grid-template-columns repeat(6, 1fr)
+    grid-auto-rows minmax(150px, auto)
     grid-gap 10px
     position relative
-    *
+    div
       background #eee
       padding 30px
       &:nth-child(even)
         background #ddd
-    header
-      grid-column 1/13
-    main
-      grid-column 4/13
-      grid-row 2/4
-    section 
-      grid-column 1/13
-      grid-row 4/6
-    aside
-    nav
-      grid-column 1/4
-    footer
-      grid-column 1/13
-
-    .grid
-      display grid
-      position absolute
-      left 0
-      top 0
-      grid-template-columns repeat(12, 1fr)
-      grid-auto-rows minmax(100% auto)
-      width 100%
-      height 100%
-      padding 0
-      opacity 0.1
-      display none
-      p
-        border 1px solid #666
-
-    .showGrid
-      display grid
+      &.div1
+        grid-column 1/3
+        grid-row 1/5
+      &.div2
+        grid-column 3/7
+        grid-row 1/3
+      &.div3
+        grid-column 3/5
+        grid-row 3/5
+      &.div4
+        grid-column 5/7
+        grid-row 3/7
+      &.div5
+        grid-column 1/5
+        grid-row 5/7
 
 </style>
